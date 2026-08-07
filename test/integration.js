995e4abadba9fdfbee4d28bf33f80f9a7599f2f3
torben-iometer <torben@iometer.de>
2026-08-07 11:43:12 +0200

    pin controllerVersion in test/integration.js to stabilize CI

--- test/integration.js
+++ test/integration.js
@@ -2,4 +2,6 @@ const path = require('path');
 const { tests } = require('@iobroker/testing');
 
 // Run integration tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
-tests.integration(path.join(__dirname, '..'));
+// controllerVersion is pinned to the latest stable js-controller release instead of the "dev" nightly build,
+// which changes daily and can be unstable (e.g. missing iobroker-data/iobroker.json after setup).
+tests.integration(path.join(__dirname, '..'), { controllerVersion: '7.2.2' });
